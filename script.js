380e243b4edd4b0901c0dc3b9410252f40bf0068
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-29 18:28:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+status = "";
+objects = [];
+mentioned_object = "";
 function preload(){
 
 }
 
 function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
 
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start(){
+    object_detector = ml5.objectDetector("cocossd", modelLoaded);
+    mentioned_object = document.getElementById("name_of_object").value;
+
+    document.getElementById("status").innerHTML = "Status: Objects Detecting";
+}
+
+function modelLoaded(){
+    status = true;
+    console.log("cocossd model is loaded, mentioned object: " + mentioned_object);
 }
 
 function draw(){
-    
+    image(video, 0, 0, 480, 380);
 }
