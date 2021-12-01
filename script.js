c981f59badbc22e69ba537835ee19467442be2da
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-12-01 18:41:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 status = "";
 objects = [];
-mentioned_object = "";
+object_name = "";
 function preload(){
 
 }
 
 function setup(){
-    canvas = createCanvas(480, 380);
+    canvas = createCanvas(680, 380);
     canvas.center();
 
     video = createCapture(VIDEO);
@@ -15,16 +15,40 @@ function setup(){
 
 function start(){
     object_detector = ml5.objectDetector("cocossd", modelLoaded);
-    mentioned_object = document.getElementById("name_of_object").value;
+    object_name = document.getElementById("name_of_object").value;
 
     document.getElementById("status").innerHTML = "Status: Objects Detecting";
 }
 
 function modelLoaded(){
     status = true;
-    console.log("cocossd model is loaded, mentioned object: " + mentioned_object);
+    console.log("cocossd model is loaded, mentioned object: " + object_name);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
 
 function draw(){
-    image(video, 0, 0, 480, 380);
+    image(video, 0, 0, 680, 380);
+    if(status){
+        object_detector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+            if(object_name == objects[i].label){
+                document.getElementById("object_status").innerHTML = object_name + " found";
+            }
+
+            fill("red");
+            text(objects[i].label + " " + floor(objects[i].confidence) + "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x,  objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
